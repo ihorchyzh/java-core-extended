80d1b39342ef4f2c0042800efcca409e54f63deb
Ihor Chyzh <ichyzh@gmail.com>
2017-03-22 21:10:56 +0200

    Add first app

---
+++ src/main/java/section0/App.java
@@ -0,0 +1,10 @@
+package section0;
+
+/**
+ * Created by ihorchyzh on 3/22/17.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Ihor!");
+    }
+}
