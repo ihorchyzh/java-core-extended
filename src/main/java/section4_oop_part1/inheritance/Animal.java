d342185aedbe274a0af0f7c2bb1b8d9ab2d5fbcf
Ihor Chyzh <ichyzh@gmail.com>
2017-04-25 22:03:45 +0300

    Add inheritance.

---
+++ src/main/java/section4_oop_part1/inheritance/Animal.java
@@ -0,0 +1,49 @@
+package section4_oop_part1.inheritance;
+
+/**
+ * Created by ihorchyzh on 4/25/17.
+ */
+public class Animal {
+    private String name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public void eat() {
+        System.out.println("Animal.eat() was called");
+    }
+
+    public void move(int speed) {
+        System.out.println("Animal.move() was called");
+        System.out.println("Animal was moving at " + speed);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/section4_oop_part1/inheritance/Main.java
@@ -0,0 +1,18 @@
+package section4_oop_part1.inheritance;
+
+/**
+ * Created by ihorchyzh on 4/25/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+        Dog dog = new Dog("Yorkie",8,20,2,4,1,20, "long silky");
+        dog.eat();
+        dog.walk();
+        dog.run();
+
+    }
+
+}
